e59767973aad1509ac00f283118c70d4181741d5
JustLittleYeti <50328199+JustLittleYeti@users.noreply.github.com>
2019-06-09 19:08:35 +0200

    Add files via upload
    Możesz dokończyć dla pozostałych linków? Dalej dla 29, 28... według wzoru

---
+++ Links.py
@@ -0,0 +1,13 @@
+class link:
+    days=0
+    link=''
+    quote=''
+
+    def show(self):
+        return self.link, self.quote
+
+###     LINKS:  ###
+link_30=link():
+link_30.days=30
+link_30.link='https://www.youtube.com/watch?v=g0kgw2kkFnM'
+link_30.quote='We were so close, and now? '
