77ace272729e6e251c9f8b816ea61fd5cdb85c30
ziny1138 <50328222+ziny1138@users.noreply.github.com>
2019-06-10 20:13:43 +0200

    naprawione
    :)

--- links.py
+++ links.py
@@ -6,12 +6,9 @@ class link:
     def show(self):
         return self.link, self.quote
 
-###     LINKS:  ###
-link_30=link()
-link_30.days=30
-link_30.link='https://www.youtube.com/watch?v=g0kgw2kkFnM'
-link_30.quote='We were so close, and now?'
 
+
+###     LINKS:  ###
 movie30=link()
 movie30.days=30
 movie30.link='https://www.youtube.com/watch?v=g0kgw2kkFnM'
@@ -20,17 +17,17 @@ movie30.quote='We were so close, and now?'
 movie29=link()
 movie29.days=29
 movie29.link='https://www.youtube.com/watch?v=MtN1YnoL46Q'
-movie29.quote='Word works this way, you never get what you want. any grapes?'
+movie29.quote='Word works this way, you never get what you want. Any grapes?'
 
 movie28=link()
 movie28.days=28
 movie28.link='https://www.youtube.com/watch?v=eY52Zsg-KVI&t=35s'
-movie28.quote='No need to be upset ;) '
+movie28.quote='No need to be upset ;)'
 
 movie27=link()
 movie27.days=27
 movie27.link='https://www.youtube.com/watch?v=w0AOGeqOnFY'
-movie27.quote='You say that it is stupid and i will agree'
+movie27.quote='You say that it\'s stupid and I will agree'
 
 movie26=link()
 movie26.days=26
@@ -40,27 +37,27 @@ movie26.quote='Because we will die. (but cats are nice)'
 movie25=link()
 movie25.days=25
 movie25.link='https://www.youtube.com/watch?v=9bZkp7q19f0'
-movie25.quote='You were eleven, and you didnt know yet, that you re fucking stupid.'
+movie25.quote='You were eleven, and you didnt know yet, that you\'re fucking stupid.'
 
 movie24=link()
 movie24.days=24
 movie24.link='https://www.youtube.com/watch?v=8f7wj_RcqYk'
-movie24.quote='Now you see that is ridicolus.'
+movie24.quote='Now you see that\'s ridicolus.'
 
 movie23=link()
 movie23.days=23
 movie23.link='https://www.youtube.com/watch?v=wZZ7oFKsKzY'
-movie23.quote='tututututuututututututututut(when you forget to close this shit and listenn to it two hours)'
+movie23.quote='tututututuututututututututut(when you forget to close this shit and listen to it for two hours)'
 
 movie22=link()
 movie22.days=22
 movie22.link='https://www.youtube.com/watch?v=5aC4akD6RUk'
-movie22.quote='litte party never killed nobody'
+movie22.quote='Litte party never killed nobody'
 
 movie21=link()
 movie21.days=21
 movie21.link='https://www.youtube.com/watch?v=OazsfQiU5k4'
-movie21.quote='Seriously, i m just done witch this word. They have no feelings. (btw, THATS A JOKE, polish pepole love Jan Paweł II)'
+movie21.quote='Seriously, I\'m just done witch this word. They have no feelings.\n (btw, THATS A JOKE, polish pepole just love Jan Paweł II)'
 
 movie20=link()
 movie20.days=20
@@ -85,12 +82,12 @@ movie17.quote='Let it go.'
 movie16=link()
 movie16.days=16
 movie16.link='https://www.youtube.com/watch?v=6Dh-RL__uN4'
-movie16.quote='Because we were talkig about yt, oh wait. (i was, you have nothing to say)'
+movie16.quote='Because we were talkig about yt, oh wait. (I was, you have nothing to say)'
 
 movie15=link()
 movie15.days=15
 movie15.link='https://www.youtube.com/watch?v=SUtziaZlDeE'
-movie15.quote='Because we were talkig about yt, oh wait. (i was, you have nothing to say)'
+movie15.quote='Because we were talkig about u, oh wait. (I was, you have nothing to say)'
 
 movie14=link()
 movie14.days=14
@@ -100,7 +97,7 @@ movie14.quote='Spider-Man, Spider-Man Does whatever a spider can(tony dies in en
 movie13=link()
 movie13.days=13
 movie13.link='https://www.youtube.com/watch?v=1415ATwMhvo'
-movie13.quote='seals are comon theme.'
+movie13.quote='Seals are comon theme.'
 
 movie12=link()
 movie12.days=12
@@ -110,22 +107,22 @@ movie12.quote='there is a lonely home, and party is on.'
 movie11=link()
 movie11.days=11
 movie11.link='https://www.youtube.com/watch?v=ty3n87vbVe4'
-movie11.quote='if you say you like Grechuta, you get 100 points on respect.'
+movie11.quote='If you say you like Grechuta, you get 100 points on respect.'
 
 movie10=link()
 movie10.days=10
 movie10.link='https://www.youtube.com/watch?v=czgV158MKb8'
-movie10.quote='You were six. and word was so simple.'
+movie10.quote='You were six and word was so simple.'
 
 movie09=link()
 movie09.days=9
 movie09.link='https://www.youtube.com/watch?v=3pTH8lb8Rc0'
-movie09.quote='the best game? the best story? (the best sad song, for sure)'
+movie09.quote='The best game? The best story? (The best sad song, for sure)'
 
 movie08=link()
 movie08.days=8
 movie08.link='https://www.youtube.com/watch?v=qYS0EeaAUMw'
-movie08.quote='you need to talk to someone? do it now. (before its to late) (and they are dead, ofc) (or dont like you)'
+movie08.quote='You need to talk to someone? do it now. (before its to late) (and they are dead, ofc) (or don\'t like you)'
 
 movie07=link()
 movie07.days=7
@@ -135,7 +132,7 @@ movie07.quote='Somewhere only we know.'
 movie06=link()
 movie06.days=6
 movie06.link='https://www.youtube.com/watch?v=SiAuAJBZuGs'
-movie06.quote='this is just nice song.'
+movie06.quote='This\'s just nice song.'
 
 movie05=link()
 movie05.days=5
@@ -145,17 +142,17 @@ movie05.quote='Work, love and die.'
 movie04=link()
 movie04.days=4
 movie04.link='https://www.youtube.com/watch?v=aNzCDt2eidg'
-movie04.quote='Sad songs are good songs.'
+movie04.quote='Sad songs\'re good songs.'
 
 movie03=link()
 movie03.days=3
 movie03.link='https://www.youtube.com/watch?v=RgKAFK5djSk'
-movie03.quote='to our dead frends, even, if they are not dead yet. '
+movie03.quote='To our dead frends, even, if they are not dead yet. '
 
 movie02=link()
 movie02.days=2
 movie02.link='https://www.youtube.com/watch?v=9E6b3swbnWg'
-movie02.quote='its fine. '
+movie02.quote='It\'s fine.'
 
 movie01=link()
 movie01.days=1
@@ -166,3 +163,5 @@ movie00=link()
 movie00.days=0
 movie00.link='https://www.youtube.com/watch?v=KybAvaM3b90'
 movie00.quote='Why i did all of this - nobody fucking cares.'
+
+movies=[movie00, movie01, movie02, movie03, movie04, movie05, movie06, movie07, movie08, movie09, movie10, movie11, movie12, movie13, movie14, movie15, movie16, movie17, movie18, movie19, movie20, movie21, movie22, movie23, movie24, movie25, movie26, movie27, movie28, movie29, movie30]
